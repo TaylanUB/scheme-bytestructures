30e00725db8b10688ec5b47efda2043192e48478
Taylan Ulrich Bayırlı/Kammer <taylanbayirli@gmail.com>
2015-09-17 22:13:15 +0200

    Simplify and fix pointer getter/setter/ref-helper.

--- guile/pointer.scm
+++ guile/pointer.scm
@@ -54,18 +54,6 @@
         (error "Tried to dereference null-pointer.")
         (ffi:pointer->bytevector (ffi:make-pointer address) content-size))))
 
-(define (%pointer-set! bytevector offset value)
-  (let ((address (cond
-                  ((integer? value)
-                   value)
-                  ((ffi:pointer? value)
-                   (ffi:pointer-address value))
-                  ((bytevector? value)
-                   (ffi:pointer-address (ffi:bytevector->pointer value)))
-                  (else
-                   (error "Pointer type failed to write:" value)))))
-    (bytevector-address-set! bytevector offset address)))
-
 (define (bs:pointer %descriptor)
   (define (get-descriptor)
     (if (promise? %descriptor)
@@ -76,35 +64,41 @@
   (define (ref-helper syntax? bytevector offset index)
     (define (syntax-list id . elements)
       (datum->syntax id (map syntax->datum elements)))
-    (let ((descriptor (get-descriptor))
-          (bytevector* (getter syntax? bytevector offset)))
-      (if (eq? '* (if syntax? (syntax->datum index) index))
+    (let* ((descriptor (get-descriptor))
+           (size (bytestructure-descriptor-size descriptor))
+           (bytevector* (if syntax?
+                            #`(%pointer-ref #,bytevector #,offset #,size)
+                            (%pointer-ref bytevector offset size)))
+           (index-datum (if syntax? (syntax->datum index) index)))
+      (if (eq? '* index-datum)
           (values bytevector* 0 descriptor)
           (if syntax?
               (bytestructure-ref-helper/syntax
-               bytevector* 0 descriptor (syntax-list index))
+               bytevector* 0 descriptor (syntax-list index index))
               (bytestructure-ref-helper*
                bytevector* 0 descriptor index)))))
   (define (getter syntax? bytevector offset)
-    (let ((size (bytestructure-descriptor-size (get-descriptor))))
-      (if syntax?
-          #`(%pointer-ref #,bytevector #,offset #,size)
-          (%pointer-ref bytevector offset size))))
+    (if syntax?
+        #`(bytevector-address-ref #,bytevector #,offset)
+        (bytevector-address-ref bytevector offset)))
   (define (setter syntax? bytevector offset value)
-    (define (syntax-car syntax)
-      (datum->syntax syntax (car (syntax->datum syntax))))
+    (define (syntax-car stx)
+      (syntax-case stx () ((car . cdr) #'car)))
     (let ((value-datum (if syntax? (syntax->datum value) value)))
       (if (and (pair? value-datum) (null? (cdr value-datum)))
-          (let ((descriptor (get-descriptor))
-                (bytevector* (getter syntax? bytevector offset)))
+          (let* ((descriptor (get-descriptor))
+                 (size (bytestructure-descriptor-size descriptor))
+                 (bytevector* (if syntax?
+                                  #`(%pointer-ref #,bytevector #,offset #,size)
+                                  (%pointer-ref bytevector offset size))))
             (if syntax?
-                (bytestructure-set!/syntax bytevector* 0 descriptor '()
-                                           (syntax-car value))
-                (bytestructure-set!* bytevector* 0 descriptor
-                                     (car value))))
+                (bytestructure-set!/syntax
+                 bytevector* 0 descriptor '() (syntax-car value))
+                (bytestructure-set!*
+                 bytevector* 0 descriptor (car value))))
           (if syntax?
-              #`(%pointer-set! #,bytevector #,offset #,value)
-              (%pointer-set! bytevector offset value)))))
+              #`(bytevector-address-set! #,bytevector #,offset #,value)
+              (bytevector-address-set! bytevector offset value)))))
   (make-bytestructure-descriptor size alignment ref-helper getter setter))
 
 ;;; pointer.scm ends here
