3d10d4459a2021696761ffae424b3eacab98641e
Taylan Ulrich Bayırlı/Kammer <taylanbayirli@gmail.com>
2015-09-20 21:59:44 +0200

    Add automated bit-field testing code for Guile.

---
+++ bitfield-tests.scm
@@ -0,0 +1,173 @@
+#!/usr/bin/env guile
+!#
+
+;;; Warning: nasal demons.
+;;;
+;;; Usage example: (test-structs (generate-structs 1000))
+;;;
+;;; Will output differences between GCC's behavior and our behavior, but not in
+;;; a very nice format.  Zero output is good.  The C code and Scheme procedure
+;;; we generate are fairly straightforward so read the code to understand.
+
+(use-modules (srfi srfi-9)
+             (ice-9 rdelim)
+             (bytestructures guile)
+             (bytestructures bytevectors))
+
+(define-record-type <struct>
+  (make-struct name fields)
+  struct?
+  (name   struct-name)
+  (fields struct-fields))
+
+(define-record-type <field>
+  (make-field name int-size bit-size signed? value)
+  struct?
+  (name     field-name)
+  (int-size field-int-size)
+  (bit-size field-bit-size)
+  (signed?  field-signed?)
+  (value    field-value))
+
+(define (generate-structs n)
+  (map random-struct (iota n)))
+
+(define (random-struct i)
+  (let ((field-count (+ 1 (random 20))))
+    (make-struct (format #f "s~a" i)
+                 (map random-field (iota field-count)))))
+
+(define (random-field i)
+  (let* ((name (format #f "f~a" i))
+         (int-size (* 8 (expt 2 (random 4))))
+         (bit-size (+ 1 (random int-size)))
+         (signed? (= 0 (random 2)))
+         (value (random (expt 2 bit-size)))
+         (value (if (and signed? (> value (+ -1 (expt 2 (- bit-size 1)))))
+                    (- value (expt 2 bit-size))
+                    value)))
+    (make-field name int-size bit-size signed? value)))
+
+(define (test-structs structs)
+  (let* ((c-code (c-code-for-structs structs))
+         (c-output (get-c-output c-code))
+         (scm-code (scm-code-for-structs structs))
+         (scm-output (get-scm-output scm-code)))
+    (diff-outputs c-output scm-output)))
+
+(define (c-code-for-structs structs)
+  (string-concatenate
+   (append
+    (list "#include <stdio.h>\n"
+          "#include <stdint.h>\n"
+          "#include <strings.h>\n"
+          "int main(){\n")
+    (map c-code-for-struct structs)
+    (list "return 0;}"))))
+
+(define (c-code-for-struct struct)
+  (let ((name (struct-name struct))
+        (fields (struct-fields struct)))
+    (string-concatenate
+     (append
+      (list (format #f "struct ~a {\n" name))
+      (map c-decl-for-field fields)
+      (list "};\n"
+            (format #f "{ struct ~a foo;\n" name)
+            (format #f "  bzero((void*)&foo, sizeof(foo));"))
+      (map c-assignment-for-field fields)
+      (list (format #f "printf(\"struct ~a:\\n\");\n" name)
+            "  uint8_t *ptr = (void*)&foo;\n"
+            "  for (int i = 0; i < sizeof(foo); ++i) {\n"
+            "    printf(\"%d \", *(ptr+i));\n"
+            "  }\n"
+            "  printf(\"\\n\");"
+            "}\n")))))
+
+(define (c-decl-for-field field)
+  (let ((name (field-name field))
+        (int-size (field-int-size field))
+        (bit-size (field-bit-size field))
+        (signed? (field-signed? field)))
+    (format #f "~aint~a_t ~a:~a; "
+            (if signed? "" "u")
+            int-size
+            name
+            bit-size)))
+
+(define (c-assignment-for-field field)
+  (let ((name (field-name field))
+        (signed? (field-signed? field))
+        (value (field-value field)))
+    (format #f "foo.~a = ~a~a;\n" name value (if signed? "" "u"))))
+
+(define (get-c-output code)
+  (let* ((port (mkstemp! (string-copy "/tmp/bitfield-XXXXXX")))
+         (file (port-filename port))
+         (exe-port (mkstemp! (string-copy "/tmp/bitfield-compiled-XXXXXX")))
+         (exe-file (port-filename exe-port))
+         (output-port (mkstemp! (string-copy "/tmp/bitfield-output-XXXXXX")))
+         (output-file (port-filename output-port)))
+    (close-port exe-port)
+    (close-port output-port)
+    (display code port)
+    (force-output port)
+    (unless (zero? (system* "gcc" "-x" "c" "-std=c11" file "-o" exe-file))
+      (error "gcc failed"))
+    (unless (zero? (system (format #f "~a > ~a" exe-file output-file)))
+      (error "exe failed"))
+    (let ((out (read-string (open output-file O_RDONLY))))
+      (for-each delete-file (list file exe-file output-file))
+      out)))
+
+(define (scm-code-for-structs structs)
+  (lambda ()
+    (string-concatenate
+     (map scm-code-for-struct structs))))
+
+(define (scm-code-for-struct struct)
+  (let* ((name (struct-name struct))
+         (fields (struct-fields struct))
+         (descriptor (struct->descriptor struct))
+         (values (map field-value fields))
+         (bs (bytestructure descriptor (list->vector values))))
+    (string-concatenate
+     (append
+      (list (format #f "struct ~a:\n" name))
+      (let ((bv (bytestructure-bytevector bs)))
+        (map (lambda (i)
+               (format #f "~a " (bytevector-u8-ref bv i)))
+             (iota (bytevector-length bv))))
+      (list "\n")))))
+
+(define (struct->descriptor struct)
+  (let ((fields (struct-fields struct)))
+    (bs:struct (map field->struct-descriptor-field fields))))
+
+(define (field->struct-descriptor-field field)
+  (let ((name (field-name field))
+        (int-size (field-int-size field))
+        (bit-size (field-bit-size field))
+        (signed? (field-signed? field)))
+    (list name
+          (module-ref (current-module)
+                      (string->symbol
+                       (format #f "~aint~a"
+                               (if signed? "" "u")
+                               int-size)))
+          bit-size)))
+
+(define (get-scm-output code)
+  (code))
+
+(define (diff-outputs o1 o2)
+  (let* ((p1 (mkstemp! (string-copy "/tmp/bitfield-out1-XXXXXX")))
+         (f1 (port-filename p1))
+         (p2 (mkstemp! (string-copy "/tmp/bitfield-out2-XXXXXX")))
+         (f2 (port-filename p2)))
+    (display o1 p1)
+    (display o2 p2)
+    (flush-all-ports)
+    (close-port p1)
+    (close-port p2)
+    (system* "diff" "-y" "--suppress-common" f1 f2)))
