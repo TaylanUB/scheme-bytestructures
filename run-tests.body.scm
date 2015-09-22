1a13061439b73c38c9dce23e4343fc97bae0ecca
Taylan Ulrich Bayırlı/Kammer <taylanbayirli@gmail.com>
2015-09-22 11:01:04 +0200

    run-tests.body.scm: Clean up again.
    The reason for bad compile times was mainly my implementation of
    SRFI-64 and nothing intrinsic to this code.  SRFI-64 has been optimized
    now, so we can remove the dirty hack.

--- run-tests.body.scm
+++ run-tests.body.scm
@@ -47,71 +47,59 @@
      (float?  (expt 2 (- size 9)))
      (signed? (- (expt 256 (- size 1))))
      (else    (- (expt 256 size) 1))))
-  (test-group "procedural"
-    (for-each
-     (lambda (descriptor-entry)
-       (destructure-numeric-descriptor-entry
-        descriptor-entry
-        (lambda (descriptor name getter setter size float? signed?)
-          (define min/max (get-min/max float? signed? size))
-          (test-group (symbol->string name)
-            (define bs (bytestructure descriptor))
-            (test-eqv "size" size (bytevector-length
-                                   (bytestructure-bytevector bs)))
-            (test-= "ref" 2
-                    (begin
-                      (setter (bytestructure-bytevector bs) 0 2)
-                      (bytestructure-ref bs)))
-            (test-= "set" 1
-                    (begin
-                      (bytestructure-set! bs 1)
-                      (getter (bytestructure-bytevector bs) 0)))
-            (test-= "min/max" min/max
-                    (begin
-                      (bytestructure-set! bs min/max)
-                      (bytestructure-ref bs)))))))
-     numeric-descriptors))
-  (maybe-skip-syntax
-   (test-group "syntactic"
-     ;; We need to use such a macro because 'define-bytestructure-accessors' only
-     ;; works when we pass it an identifier that's bound at the top-level.  On
-     ;; the other hand we want to keep its size minimal, because it inflates
-     ;; compilation time.  Therefore the procedural tests are separated.
-     (define-syntax test-numeric-descriptors/syntactic
-       (syntax-rules ()
-         ((_ <descriptor-id> ...)
-          (begin
-            (test-group (symbol->string '<descriptor-id>)
-              (destructure-numeric-descriptor-entry
-               (assq <descriptor-id> numeric-descriptors)
-               (lambda (descriptor name getter setter size float? signed?)
-                 (define min/max (get-min/max float? signed? size))
-                 ;; Must insert the top-level reference <descriptor-id> here.
-                 (define-bytestructure-accessors <descriptor-id>
-                   bs-ref-helper bs-getter bs-setter)
-                 (define bv (make-bytevector size))
-                 (test-= "ref" 2
-                         (begin
-                           (setter bv 0 2)
-                           (bs-getter bv)))
-                 (test-= "set" 1
-                         (begin
-                           (bs-setter bv 1)
-                           (getter bv 0)))
-                 (test-= "min/max" min/max
-                         (begin
-                           (bs-setter bv min/max)
-                           (bs-getter bv))))))
-            ...))))
-     (test-numeric-descriptors/syntactic
-      float32 float32le float32be
-      double64 double64le double64be
-      int8 int16 int32 int64
-      int16le int32le int64le
-      int16be int32be int64be
-      uint8 uint16 uint32 uint64
-      uint16le uint32le uint64le
-      uint16be uint32be uint64be))))
+  (define-syntax test-numeric-descriptors
+    (syntax-rules ()
+      ((_ <descriptor-id> ...)
+       (begin
+         (destructure-numeric-descriptor-entry
+          (assq <descriptor-id> numeric-descriptors)
+          (lambda (descriptor name getter setter size float? signed?)
+            (test-group (symbol->string name)
+              (test-group "procedural"
+                (define min/max (get-min/max float? signed? size))
+                (define bs (bytestructure descriptor))
+                (test-eqv "size" size (bytevector-length
+                                       (bytestructure-bytevector bs)))
+                (test-= "ref" 2
+                        (begin
+                          (setter (bytestructure-bytevector bs) 0 2)
+                          (bytestructure-ref bs)))
+                (test-= "set" 1
+                        (begin
+                          (bytestructure-set! bs 1)
+                          (getter (bytestructure-bytevector bs) 0)))
+                (test-= "min/max" min/max
+                        (begin
+                          (bytestructure-set! bs min/max)
+                          (bytestructure-ref bs))))
+              (test-group "syntactic"
+                (define min/max (get-min/max float? signed? size))
+                ;; Must insert the top-level reference <descriptor-id> here.
+                (define-bytestructure-accessors <descriptor-id>
+                  bs-ref-helper bs-getter bs-setter)
+                (define bv (make-bytevector size))
+                (test-= "ref" 2
+                        (begin
+                          (setter bv 0 2)
+                          (bs-getter bv)))
+                (test-= "set" 1
+                        (begin
+                          (bs-setter bv 1)
+                          (getter bv 0)))
+                (test-= "min/max" min/max
+                        (begin
+                          (bs-setter bv min/max)
+                          (bs-getter bv)))))))
+         ...))))
+  (test-numeric-descriptors
+   float32 float32le float32be
+   double64 double64le double64be
+   int8 int16 int32 int64
+   int16le int32le int64le
+   int16be int32be int64be
+   uint8 uint16 uint32 uint64
+   uint16le uint32le uint64le
+   uint16be uint32be uint64be))
 
 (test-group "vector"
   (test-assert "create" (bs:vector 3 uint16))
