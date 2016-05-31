c78c1cdf06d6c7767703fb47b9101e4ab7340171
Taylan Ulrich Bayırlı/Kammer <taylanbayirli@gmail.com>
2016-06-01 00:28:25 +0300

    bytestructures/guile/base.scm: Add record printers.

--- bytestructures/guile/base.scm
+++ bytestructures/guile/base.scm
@@ -6,3 +6,15 @@
  (bytestructures guile utils))
 (include-from-path "bytestructures/body/base.scm")
 (include-from-path "bytestructures/r7/base.exports.sld")
+
+(import (srfi srfi-9 gnu))
+
+(set-record-type-printer!
+ <bytestructure-descriptor>
+ (lambda (record port)
+   (format port "#<bytestructure-descriptor 0x~x>" (object-address record))))
+
+(set-record-type-printer!
+ <bytestructure>
+ (lambda (record port)
+   (format port "#<bytestructure 0x~x>" (object-address record))))
